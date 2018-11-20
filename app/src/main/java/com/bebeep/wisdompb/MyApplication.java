5d0756280bc452a5122f4865f49396340a40bc1c
bebeep <424468648@qq.com>
2018-11-20 20:44:04 +0800

    123

--- app/src/main/java/com/bebeep/wisdompb/MyApplication.java
+++ app/src/main/java/com/bebeep/wisdompb/MyApplication.java
@@ -13,7 +13,7 @@ public class MyApplication extends Application {
     public static Gson gson;
     public static Context context;
 
-    //123456
+    //123456789
 
     public static MyApplication getInstance() {
         if(instance == null){
